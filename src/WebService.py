33e839cab68094618d9829326c5f49098c367767
Hugo Cuellar <dev.hugo.cuellar@gmail.com>
2020-11-30 19:44:39 -0600

    nuevos metodos

--- src/WebService.py
+++ src/WebService.py
@@ -1,13 +1,18 @@
 #!/bin/python3
 
-from flask import Flask, request, Response
-import requests, json, urllib.parse
+from flask import Flask, request, Response, jsonify
+import requests, urllib.parse
 from bs4 import BeautifulSoup
 from markupsafe import escape
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from PIL import Image
 import io
+import numpy as np
+import matplotlib.pyplot as plt
+import base64
+from sklearn.linear_model import LinearRegression
+import json
 
 app = Flask(__name__)
 
@@ -120,6 +125,88 @@ def consumo():
         return "<b>Se mando: título </b> %s" %data['titulo'] + ", <b> descripción: </b> %s " %data['descripcion']  + "  <b> calorías: </b> %i" %data['caloria'] + " <b> fecha: </b> %s" %data['fecha']
     else:
         return "Not found method"
+
+@app.route("/reglin/", methods=["GET", "POST"])
+def predecir():
+    if request.method == "POST":
+        data = request.get_json()
+        if (data != None) and ('x' in data) and (type(data['x']) is list) and ('y' in data) and (type(data['y']) is list):
+            valores_x = np.array(data['x'])
+            valores_y = np.array(data['y'])
+            # Entrenamiento
+            regresion_lineal = LinearRegression()
+            regresion_lineal.fit(valores_x.reshape(-1,1), valores_y)
+
+            modelo_w = regresion_lineal.coef_
+            modelo_b = regresion_lineal.intercept_
+
+            # Realiza la prediccion
+            if ('predicciones' in data) and int(data['predicciones']) > 0:
+                numero_predicciones = int(data['predicciones'])
+                maximo_x = valores_x.max()
+                inicio_nuevos_x = maximo_x + 1
+                fin_nuevos_x = maximo_x + numero_predicciones + 1
+                step_nuevos = 1
+                nuevos_x = np.arange(inicio_nuevos_x, fin_nuevos_x, step_nuevos)
+                predicciones_y = regresion_lineal.predict(nuevos_x.reshape(-1,1))
+                # Grafico
+                plt.scatter(nuevos_x, predicciones_y, label="data", color="blue")
+                plt.title("Predicciones")
+                picIObytes = io.BytesIO()
+                plt.savefig(picIObytes, format="png")
+                picIObytes.seek(0)
+                imgBase64Data = base64.b64encode(picIObytes.read())
+                imgAsStr = imgBase64Data.decode('ascii')
+                # Respuesta a devolver
+                respuesta = {
+                    "status": True,
+                    "data": {
+                        "x": nuevos_x.tolist(),
+                        "y": predicciones_y.tolist(),
+                        "chart": imgAsStr
+                    }
+                }
+                return jsonify(respuesta)
+            else:
+                return "error"
+        else:
+            respuesta = {
+                "status": False,
+                "message": 'Error en la petición. Usa { x:[1,2, ... n], y:[1,2,3 ... n], predicciones: num }'
+            }
+            return jsonify(respuesta)
+    elif request.method == "GET":
+        return "Hola, GET!"
+
+@app.route('/rlineal', methods=['POST','GET'])
+def preRLinea():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        vx = data['vx']
+        vy = data['vy']
+        x = data['x']
+        #return "y_obj: " + str(controller.getRLineal(vx, vy, x))
+        return getRLineal(vx, vy, x)
+    else:
+        return "not found"
+
+def getRLineal(v_x, v_y, x_pos):
+    #v_x = [2014, 2015, 2016, 2017, 2018, 2019]
+    #v_y = [530, 560, 610, 690, 720, 830]
+    #x_pos = 202_0
+    n = len(v_x)
+    x, y, xy, xx = [0.0 for _ in range(4)]
+    for i in range(n):
+        x += v_x[i]
+        y += v_y[i]
+        xy += v_x[i] * v_y[i]
+        xx += v_x[i] ** 2
+    m = ((n * xy) - (x * y)) / ((n * xx) - (x ** 2))
+    b = (y - (m * x)) / n
+    y_obj = (m * x_pos) + b
+    return json.dumps({"status": "ok", "y_obj":y_obj})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
